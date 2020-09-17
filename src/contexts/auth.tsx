3e2a4254973f6e4a4a0cfb4004a7a234f6196792
unknown <delciney07@gmail.com>
2020-09-16 21:38:01 -0300

    usando o localStorage para manter o usuario logado

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,21 +1,35 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import * as auth from "../sevices/auth";
 interface AuthContextData {
   signed: boolean;
   user: object | null;
   singIn(): Promise<void>;
+  singOut(): void;
 }
 
 const AuthContexts = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<object | null>(null);
+  useEffect(() => {
+    const storageUser = localStorage.getItem("@RNAuth:user");
+    const storageToken = localStorage.getItem("@RNAuth:token");
+
+    if (storageUser && storageToken) {
+      setUser(JSON.parse(storageUser));
+    }
+  }, []);
   async function singIn() {
     const response = await auth.singIn();
     setUser(response.user);
+    localStorage.setItem("@RNAuth:user", JSON.stringify(response.user));
+    localStorage.setItem("@RNAuth:token", response.token);
+  }
+  function singOut() {
+    setUser(null);
   }
   return (
-    <AuthContexts.Provider value={{ signed: !!user, user, singIn }}>
+    <AuthContexts.Provider value={{ signed: !!user, user, singIn, singOut }}>
       {children}
     </AuthContexts.Provider>
   );
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../../contexts/auth";
 import "./index.css";
 const Dashboard: React.FC = () => {
+  const { singOut } = useContext(AuthContext);
+  function handleSingOut() {
+    singOut();
+  }
   return (
     <div className="Dashboard">
       <h1>Dashboard</h1>
+      <button onClick={handleSingOut}>Logout</button>
     </div>
   );
 };
